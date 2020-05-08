62e8e77164f04f945c63d32e1a6c6ce4c06329b7
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2020-05-08 13:53:55 -0300

    Dashboard done

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+
+import LogoImg from '../../assets/logo.svg'
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>
+  return (
+    <>
+      <img src={LogoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório." />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/54302847?s=460&v=4"
+            alt="Nathan Vieira Marcelino"
+          />
+          <div>
+            <strong>GoBarber-backend</strong>
+            <p>Backend da aplicação GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/54302847?s=460&v=4"
+            alt="Nathan Vieira Marcelino"
+          />
+          <div>
+            <strong>GoBarber-backend</strong>
+            <p>Backend da aplicação GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://avatars1.githubusercontent.com/u/54302847?s=460&v=4"
+            alt="Nathan Vieira Marcelino"
+          />
+          <div>
+            <strong>GoBarber-backend</strong>
+            <p>Backend da aplicação GoBarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
